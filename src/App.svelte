677cd34d7e8a648973079727381060e0819b02f8
James Hall <jdhall@live.co.uk>
2021-04-18 07:06:44 +0100

    the basic functionality and placement of the todos

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,36 @@
 <script>
-  export let name;
   import Todo from "./todo.svelte";
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <div class="container">
+    <div id="top-right">Need to do Quickly</div>
+    <div id="top-left">Should be done quickly</div>
+
+    <div id="bottom-left">Needs to be done</div>
+    <div id="bottom-right">Not really needed</div>
+  </div>
   <Todo />
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  #top-right {
+    background-color: #ff3e00;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  #top-left {
+    background-color: aqua;
+  }
+
+  #bottom-left {
+    background-color: yellow;
+  }
+  .container {
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+    width: 100vw;
   }
 
   @media (min-width: 640px) {
--- src/todo.svelte
+++ src/todo.svelte
@@ -1,17 +1,22 @@
 <script>
   export let tasks = [];
+  let task;
+  let importance = 0;
+  let urgency = 0;
 
+  let coord = { x: 0, y: 0 };
   let uid = 1;
   function add(input) {
     const task = {
       id: uid++,
       done: false,
-      importance: select.value,
-      description: input.value,
+      importance: importance,
+      urgency: urgency,
+      description: input,
     };
 
     tasks = [task, ...tasks];
-    input.value = "";
+    input = "";
   }
 
   function remove(task) {
@@ -25,20 +30,44 @@
   }
 </script>
 
-<input
-  type="text"
-  bind:group={task}
-  on:keydown={(e) => e.key === "Enter" && add(e.target)}
-/>
-<label>
-  <select bind:group={task} value="1" />
-</label>
+<div class="input">
+  <h1>What tasks do you need to accomplish?</h1>
+  <form on:submit|preventDefault={add(task)}>
+    <input
+      type="text"
+      bind:value={task}
+      on:keydown={(e) => e.key === "Enter"}
+    />
+    <label>
+      <input type="range" bind:value={importance} min="0" max="100" />
+    </label>
+    <label>
+      <input type="range" bind:value={urgency} min="0" max="100" />
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+</div>
 
-<h2>todo</h2>
 {#each tasks.filter((t) => !t.done) as todo (todo.id)}
-  <label>
+  <label class="to" style="left: {todo.importance}%; top:{todo.urgency}%">
     <input type="checkbox" on:change={() => mark(todo, true)} />
     {todo.description}
+
     <button on:click={() => remove(todo)}>remove</button>
   </label>
 {/each}
+
+<style>
+  .to {
+    position: absolute;
+    border: 3px solid green;
+    background-color: hotpink;
+  }
+  .input {
+    position: absolute;
+    top: 50vh;
+    left: 50vw;
+    transform: translate(-50%, -50%);
+    justify-items: center;
+  }
+</style>
